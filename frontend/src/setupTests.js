ec4adc5308fdae9fa65e6e7575ac523f237fb187
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-01 10:56:05 +0500

    Add frontend setupTests.js - Jest test setup

---
+++ frontend/src/setupTests.js
@@ -0,0 +1,9 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// Urdu: NFM-X setupTests.js
+// یہ Jest tests ke liye setup file hai
+// Custom matchers add kiye gaye hain DOM nodes ke liye
