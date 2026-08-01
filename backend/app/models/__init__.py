9de2254fac1636a712a90088b3c745bbe4b239dd
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-01 11:56:19 +0500

    Add models package __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,13 @@
+"""
+NFM-X Models Package
+======================
+
+This package contains all data models for NFM-X.
+"""
+
+from .memory_models import *
+from .ocr_models import *
+
+__all__ = []
+
+# Urdu: NFM-X ماڈلز پیکج
