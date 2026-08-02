a77bec044e5c3958a33b1c56844831a9efcc0038
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-03 03:44:32 +0500

    feat(v2): add index files for frontend V2 directories

---
+++ frontend/src/components/V2/index.ts
@@ -0,0 +1,3 @@
+// V2 Components exports
+export { default as MemoryGraphV2 } from "./MemoryGraphV2";
+export { default as ConflictResolverV2 } from "./ConflictResolverV2";
---
+++ frontend/src/pages/V2/index.ts
@@ -0,0 +1,4 @@
+// V2 Pages exports
+export { default as MemoryV2Page } from "./MemoryV2Page";
+export { default as GraphV2Page } from "./GraphV2Page";
+export { default as ConflictsV2Page } from "./ConflictsV2Page";
